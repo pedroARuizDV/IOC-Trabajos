21351a871020d717d2d1d4ee0d3b712f3f503279
pedroARuizDV <100480529+pedroARuizDV@users.noreply.github.com>
2022-02-26 16:30:42 +0100

    Add files via upload

---
+++ M07/U3/ENTREGA/M07_EAC3_E3/M07_EAC3_E3/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M07_EAC3_E3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
